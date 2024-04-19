e2082c2a5ddce93f5e2ac5057e379637c8fca349
yLexter <lucasmaia5954@gmail.com>
2024-04-19 20:14:34 -0300

    feat: add new controllers

---
+++ APIs/AcademicSystem/controllers/AdminController.ts
@@ -0,0 +1,87 @@
+import express, { Request, Response, Router } from "express";
+import { Student, Teacher } from "../initSequelize";
+
+const adminRouter = Router();
+
+// Students endpoints
+adminRouter.get("/students", async (req: Request, res: Response) => {
+   try {
+      const students = await Student.findAll();
+
+      return res.json({
+         students: students,
+      });
+   } catch (error) {
+      const message = (error as Error).message;
+      res.json({ error: `An Error Ocorrued: ${message}` });
+   }
+});
+
+adminRouter.post("/students", (req: Request, res: Response) => {
+   // Logic to add a new student
+   res.send("Add Student");
+});
+
+adminRouter.delete("/students/:id", (req: Request, res: Response) => {
+   // Logic to remove a student by ID
+   res.send("Remove Student");
+});
+
+// Teachers Endpoints
+
+adminRouter.get("/teachers", async (req: Request, res: Response) => {
+   try {
+      const teachers = await Teacher.findAll();
+
+      return res.json({
+         teachers: teachers,
+      });
+   } catch (error) {
+      const message = (error as Error).message;
+      res.json({ error: `An Error Ocorrued: ${message}` });
+   }
+});
+
+adminRouter.post("/teachers", (req: Request, res: Response) => {
+   // Logic to add a new professor
+   res.send("Add Professor");
+});
+
+adminRouter.delete("/teachers/:id", (req: Request, res: Response) => {
+   // Logic to remove a professor by ID
+   res.send("Remove Professor");
+});
+
+// Class
+adminRouter.get("/classes", (req: Request, res: Response) => {
+   // Logic to view registered classes
+   res.send("View Classes");
+});
+
+adminRouter.post("/classes", (req: Request, res: Response) => {
+   // Logic to add a new class
+   res.send("Add Class");
+});
+
+adminRouter.delete("/classes/:id", (req: Request, res: Response) => {
+   // Logic to delete a class by ID
+   res.send("Delete Class");
+});
+
+// Class Teachers endpoints
+adminRouter.get("/classrooms", (req: Request, res: Response) => {
+   // Logic to view registered classrooms
+   res.send("View Classrooms");
+});
+
+adminRouter.post("/classrooms", (req: Request, res: Response) => {
+   // Logic to add a new classroom
+   res.send("Add Classroom");
+});
+
+adminRouter.get("/class-schedules", (req: Request, res: Response) => {
+   // Logic to view class schedules
+   res.send("View Class Schedules");
+});
+
+export { adminRouter };
---
+++ APIs/AcademicSystem/controllers/TeacherController.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, Router } from "express";
+
+const teacherRouter = Router();
+// Professor endpoints
+teacherRouter.post("/lectures", (req: Request, res: Response) => {
+   // Logic to register a lecture
+   res.send("Register Lecture");
+});
+
+teacherRouter.post("/grades", (req: Request, res: Response) => {
+   // Logic to post student grades
+   res.send("Post Grades");
+});
+
+teacherRouter.post("/final-grades", (req: Request, res: Response) => {
+   // Logic to post final grades of students
+   res.send("Post Final Grades");
+});
+
+teacherRouter.get("/class-report", (req: Request, res: Response) => {
+   // Logic to view a class report
+   res.send("View Class Report");
+});
+
+teacherRouter.get("/lecture-records", (req: Request, res: Response) => {
+   // Logic to view lecture records
+   res.send("View Lecture Records");
+});
+
+export { teacherRouter };
